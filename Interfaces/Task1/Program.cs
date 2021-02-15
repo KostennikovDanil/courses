73bafbb55b431b990d5c38f726a64c091d4291c7
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2021-02-15 15:36:15 +0600

    Add

---
+++ Interfaces/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalculator calculator = new Calculator();
+            Console.WriteLine("Enter to numbers");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sum is {0}",calculator.Sum(a, b));
+            }
+            catch (Exception ex)
+            {
+               // Console.WriteLine(ex.Message);
+            }
+        }
+
+        public interface ICalculator
+        {
+            public int Sum(int a, int b);
+        }
+
+        public class Calculator : ICalculator
+        {
+            int ICalculator.Sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+    }
+}
---
+++ Interfaces/Task2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new Logger();
+            ICalculator calculator = new Calculator(logger);
+            Console.WriteLine("Enter to numbers");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                calculator.Sum(a, b);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+    }
+
+    public interface ILogger
+    {
+        public void Error(string message);
+        public void Event(string message);
+
+    }
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+        }
+
+        public void Event(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(message);
+        }
+    }
+
+
+    public interface ICalculator
+    {
+        public void Sum(int a, int b);
+    }
+
+    public class Calculator : ICalculator
+    {
+        ILogger Logger { get; }
+        public Calculator(ILogger logger)
+        {
+            Logger = logger;
+        }
+        void ICalculator.Sum(int a, int b)
+        {
+            Logger.Event(Convert.ToString(a+b));;
+        }
+    }
+}
