21d604df29071d3d7f1490bf289ae9f00c7b8359
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2021-04-16 17:16:15 +0600

    add

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Collections
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string path = Console.ReadLine();
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            try
+            {
+                string text;
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    text = reader.ReadToEnd();
+                }
+
+                var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray()).Split(" ");
+
+                foreach (string word in noPunctuationText)
+                {
+                    if (words.ContainsKey(word))
+                    {
+                        words[word]++;
+                    }
+                    else
+                    {
+                        words.Add(word, 1);
+                    }
+                }
+
+                string[] topWords = new string[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    topWords[i] = words.Keys.ElementAt(i);
+                }
+
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9 - i; j++)
+                    {
+                        if (words[topWords[j]] < words[topWords[j + 1]])
+                        {
+                            string tmp = topWords[j];
+                            topWords[j] = topWords[j + 1];
+                            topWords[j + 1] = tmp;
+                        }
+                    }
+                            
+                }
+
+
+                for (int i = 10; i < words.Count; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if(words.Values.ElementAt(i)> words[topWords[j]])
+                        {
+                            for (int k = 9; k > j; k--)
+                            {
+                                topWords[k] = topWords[k - 1];
+                            }
+                            topWords[j] = words.Keys.ElementAt(i);
+                            break;
+                        }
+                    }
+                }
+
+                foreach (var word in topWords)
+                {
+                    Console.WriteLine(words[word]);
+                }
+
+            }
+
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ Collections/Comparison/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+            List<string> list = new List<string>();
+
+
+            string path = Console.ReadLine();
+
+            var stopWatch = Stopwatch.StartNew();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string str = "";
+                    while ((str = reader.ReadLine()) != null) 
+                    {
+                        linkedList.AddLast(str);
+                    }
+
+                    Console.WriteLine($"Time of adding to linkedList {linkedList.Count} elements = {stopWatch.Elapsed.TotalMilliseconds} ms");
+
+                    reader.DiscardBufferedData();
+                    reader.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+
+                    stopWatch.Restart();
+
+                    while ((str = reader.ReadLine()) != null)
+                    {
+                        list.Add(str);
+                    }
+
+                    Console.WriteLine($"Time of adding to list {list.Count} elements = {stopWatch.Elapsed.TotalMilliseconds} ms");
+                }
+
+
+            }
+            
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+           
+        }
+    }
+}
